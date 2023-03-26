36a7d3cec4acbb4e3cc7d04310d8c56bc0a13335
Chandra Annamaneni <chandra627@gmail.com>
2023-03-25 23:20:41 -0700

    prime_gen will now take max as the first argument for 2^64 - 1.

--- src/prime_gen.cpp
+++ src/prime_gen.cpp
@@ -9,6 +9,7 @@
 #include <mutex>
 #include <queue>
 #include <algorithm>
+#include <limits>
 
 #include "primes.h"
 #include "queue_dispatch.h"
@@ -18,7 +19,7 @@ void usage(const char* prog_name)
 {
     std::cout << prog_name << " <exponent> " << "[-f <output file name>]" 
     << std::endl;
-    std::cout << "exponent should be between 1 and 18" <<std::endl;
+    std::cout << "exponent should be between 1 and 19 or max" <<std::endl;
 }
 
 int main(int argc, char *argv[])
@@ -28,8 +29,14 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    unsigned int exp = std::strtoull(argv[1], nullptr, 10);
-    if ((exp < 1) && (exp > 18)) {
+    bool isMax = false;
+    unsigned int exp;
+    if (std::string(argv[1]) == std::string("max"))
+        isMax = true;
+    else 
+        exp = std::strtoull(argv[1], nullptr, 10);
+
+    if ((exp < 1) || (exp > 19)) {
         usage(argv[0]);
         return -1;
     }
@@ -50,7 +57,9 @@ int main(int argc, char *argv[])
 
     ulong max_prime = powl(10, p_exp);
     ulong_vec prime_seeds;
+    std::cout << "Generating first primes" << std::endl;
     primes::generate_first_primes(max_prime, prime_seeds);
+    std::cout << "Done with first primes" << std::endl;
 
     if (output_file != "")
         file_writer::write_file(prime_seeds, output_file);
@@ -61,10 +70,11 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    int num_cores = std::thread::hardware_concurrency();
     ulong prime_factor_limit = max_prime;
-    max_prime = powl(10, exp);
-    queue_dispatch range_q(prime_factor_limit, max_prime, num_cores);
+    max_prime = (isMax == true) ? std::numeric_limits<uint64_t>::max()
+                                  :powl(10, exp);
+
+    queue_dispatch range_q(prime_factor_limit, max_prime);
     range_q.fill_queue();
     file_writer *fw = nullptr;
     if (output_file != "") {
@@ -77,7 +87,7 @@ int main(int argc, char *argv[])
 
     std::vector<primes*> prime_ranges;
     std::vector<std::thread> thread_list;
-    for (int i = 0; i < num_cores; i++) {
+    for (int i = 0; i < std::thread::hardware_concurrency(); i++) {
         primes *pm = new primes(i, range_q, prime_seeds, fw);
         prime_ranges.push_back(pm);
         thread_list.push_back(std::thread(&primes::fetch_ranges, pm));
