5f70c4a373a9984c1a0641f695d3910d7d23345b
Chandra Annamaneni <chandra627@gmail.com>
2023-06-04 08:16:33 -0700

    Final version of aks.

---
+++ util/combi3.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <climits>
+
+using vec_64 = std::vector<uint64_t>;
+using pair_64 = std::pair<uint64_t, uint64_t>;
+using pair_64_bl = std::pair<uint64_t, bool>;
+using pair_vec = std::vector<pair_64_bl>;
+
+bool reduce(pair_64& numtor, uint64_t den_end);
+bool test_number(uint64_t num);
+void test_single_input(uint64_t inp);
+void generate_list(uint64_t inp);
+void usage(const char* prog_name);
+
+int main(int argc, char *argv[])
+{
+    if ((argc != 2) && (argc != 3)) {
+        usage(argv[0]);
+        return -1;
+    }
+
+    uint64_t t_num = std::strtoll(argv[argc - 1], nullptr, 10);
+    if (argc == 3)  {
+        if (std::string(argv[1]) == "-l") {
+            generate_list(t_num);
+        } else {
+            usage(argv[0]);
+            return -1;
+        }
+    }
+
+    if (argc == 2)
+        test_single_input(t_num);
+}
+
+void generate_list(uint64_t inp)
+{
+    std::cout << "2\n3\n5\n";
+    for (uint64_t i = 6 ; i < inp; i += 6) {
+        uint64_t m = i - 1;
+        uint64_t n = i + 1;
+        if (m % 5 != 0) 
+            if (test_number(m) == true)
+                std::cout << m << std::endl;
+
+        if (n % 5 != 0)
+            if (test_number(n) == true)
+                std::cout << n << std::endl;
+    }
+}
+
+void test_single_input(uint64_t inp)
+{
+    if (test_number(inp) == true)
+        std::cout << "prime" << std::endl;
+    else
+        std::cout << "not a prime" << std::endl;
+}
+
+bool test_number(uint64_t num)
+{
+    bool is_prime = true;
+    uint64_t lim = (num + 1)/2;
+    for (int i = 2; i <= lim; i++) { 
+        pair_64 d = pair_64(num - 1 + 1, num);
+        if (reduce(d, i) == false)  {
+            is_prime = false;
+            break;
+        }
+    }
+    return is_prime;
+}
+
+bool reduce(pair_64& numtor, uint64_t den_end)
+{
+    uint64_t st = numtor.first;
+    pair_vec num_pair_vec;
+    for (int i = numtor.first; i <= numtor.second; i++) 
+        num_pair_vec.push_back({st++, false});
+
+    for (uint64_t den = 2; den <= den_end; den++) {
+        uint64_t rem = numtor.first % den;
+        uint64_t t = (rem == 0) ? 0:(den - rem);
+        while (t < num_pair_vec.size()) {
+            if (num_pair_vec[t].second == false) {
+                num_pair_vec[t].first = num_pair_vec[t].first / den;
+                num_pair_vec[t].second = true;
+                break;
+            } else 
+                t += den;
+        }
+    }
+   
+    return (!num_pair_vec[num_pair_vec.size() - 1].second);
+}
+
+void usage(const char* prog_name)
+{
+    std::cout << std::string(prog_name) + " [-l] <number>" << std::endl;
+    std::cout << "if '-l' is the first argument, generates primes upto ";
+    std::cout << "the given number" << std::endl;
+    std::cout << "Otherwise tests the given number for primality" << std::endl;
+}
